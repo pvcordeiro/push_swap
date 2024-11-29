ec2430911e0accbbea1e7aabd3b76dfed4edceec
Paulo Victor <pv.35@hotmail.com>
2024-11-29 19:57:16 +0000

    adsa

--- radix_prep.c
+++ radix_prep.c
@@ -6,23 +6,23 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 18:15:41 by paude-so          #+#    #+#             */
-/*   Updated: 2024/11/28 12:14:46 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/11/29 19:28:23 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	calculate_ranks(t_stack **stack, int *ranks)
+static void	calculate_ranks(t_stack **a, int *ranks)
 {
 	t_stack	*tmp;
 	t_stack	*cmp_node;
 	int		cmp_rank;
 
-	tmp = *stack;
+	tmp = *a;
 	while (tmp)
 	{
 		cmp_rank = 0;
-		cmp_node = *stack;
+		cmp_node = *a;
 		while (cmp_node)
 		{
 			if (cmp_node->number < tmp->number)
@@ -34,11 +34,11 @@ static void	calculate_ranks(t_stack **stack, int *ranks)
 	}
 }
 
-static void	update_stack_with_ranks(t_stack **stack, int *ranks)
+static void	update_stack_with_ranks(t_stack **a, int *ranks)
 {
 	t_stack	*tmp;
 
-	tmp = *stack;
+	tmp = *a;
 	while (tmp)
 	{
 		tmp->number = *ranks++;
@@ -46,14 +46,14 @@ static void	update_stack_with_ranks(t_stack **stack, int *ranks)
 	}
 }
 
-void	normalize_stack(t_stack **stack)
+void	normalize_stack(t_stack **a)
 {
 	int	*ranks;
 	int	size;
 
-	size = stack_size(stack);
+	size = stack_size(a);
 	ranks = malloc(size * sizeof(int));
-	calculate_ranks(stack, ranks);
-	update_stack_with_ranks(stack, ranks);
+	calculate_ranks(a, ranks);
+	update_stack_with_ranks(a, ranks);
 	free(ranks);
 }
